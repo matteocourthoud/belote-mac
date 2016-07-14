692c40e06872e23472e8908c5618375a333d22b3
Matteo Courthoud <Matteo@matteo-mbp.lan>
2016-07-14 09:05:20 +0200

    Initial Commit

---
+++ Belote/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Belote
+//
+//  Created by Matteo Courthoud on 14/07/16.
+//  Copyright © 2016 Matteo Courthoud. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Belote/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Belote
+//
+//  Created by Matteo Courthoud on 14/07/16.
+//  Copyright © 2016 Matteo Courthoud. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
